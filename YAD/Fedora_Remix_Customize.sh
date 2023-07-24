ca86271c3d4392cca47fc907ae6d20154b1b89ab
Travis Michette <tmichett@redhat.com>
2023-07-24 15:39:32 -0400

    Updated script and tested

--- YAD/Fedora_Remix_Customize.sh
+++ YAD/Fedora_Remix_Customize.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 yad --title "Fedora Remix Desktop Customization Tool" --form --columns=3 --width=540 --height=190 --text="Travis's Fedora Remix Customization Utility" --image=/var/www/html/files/boot/splash.png  \
---field="<b>Update System Packages</b>":fbtn "sudo bash '/opt/FedoraRemix/scripts/update_pkgs.sh'" \
+--field="<b>Update System Packages</b>":fbtn "terminator -e 'sudo bash /opt/FedoraRemix/scripts/update_pkgs.sh'" \
 --field="<b>Customize Stuff</b>":fbtn "konsole --noclose -e sh './scripts/arch'" \
 --button=Exit:1
