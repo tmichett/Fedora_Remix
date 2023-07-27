e78266728bcb2aa9f5fe892ffb4704eb17352b9c
Travis Michette <tmichett@redhat.com>
2023-07-27 17:31:44 -0400

    Updated YAD Subscripts

--- YAD/Supporting_Scripts/customize_gnome_desktop.sh
+++ YAD/Supporting_Scripts/customize_gnome_desktop.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-mkdir -p ~/.local/share/gnome-shell/extensions/ding@rastersoft.com
-cd ~/.local/share/gnome-shell/extensions/ding@rastersoft.com
-pwd
-unzip -q /opt/FedoraRemix/files/dingrastersoft.com.v57.shell-extension.zip -d ~/.local/share/gnome-shell/extensions/ding@rastersoft.com
 gnome-extensions enable ding@rastersoft.com
