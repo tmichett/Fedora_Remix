c3368f706c3c6a4e06cd08b8525da3ea199f707f
Travis Michette <tmichett@redhat.com>
2023-08-27 12:13:12 -0400

    Updated and tested script

--- YAD/scripts/create_pxe_server.sh
+++ YAD/scripts/create_pxe_server.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 USER="$(whoami)"
 cd /opt/PXEServer/
+ansible-galaxy collection install -r collections/requirements.yml -p collections
+ansible-galaxy install -r roles/requirements.yml -p roles
 ansible-playbook Setup_PXE_Server.yml  -k
