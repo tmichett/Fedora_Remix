57deca2243a21e5d25bde94d0e3778ac16842ba2
Travis Michette <tmichett@gmail.com>
2024-06-29 14:55:16 -0400

    Created VSCode deploy extensions

---
+++ YAD/scripts/deploy_vscode_extensions.sh
@@ -0,0 +1,7 @@
+
+
+#!/bin/bash
+code --install-extension ChrisChinchilla.vale-vscode-0.21.0.vsix redhat.ansible-24.9.320163.vsix
+code --install-extension redhat.vscode-yaml-1.15.0.vsix
+code --install-extension flobilosaurus.vscode-asciidoc-slides-1.3.0.vsix
+code --install-extension asciidoctor.asciidoctor-vscode-3.3.1.vsix
