8f36999d5dd26b65c854aeb9052cd9d5a02f94c7
Travis Michette <tmichett@redhat.com>
2025-04-19 15:28:30 -0400

    FIX: Updated deploy vscode extensions

--- YAD/scripts/deploy_vscode_extensions.sh
+++ YAD/scripts/deploy_vscode_extensions.sh
@@ -5,7 +5,6 @@ code --install-extension /opt/FedoraRemix/VSCode/aaron-bond.better-comments-3.0.
 code --install-extension /opt/FedoraRemix/VSCode/adpyke.codesnap-1.3.4.vsix
 code --install-extension /opt/FedoraRemix/VSCode/asciidoctor-vscode-3.4.2.vsix
 code --install-extension /opt/FedoraRemix/VSCode/ChrisChinchilla.vale-vscode-0.21.0.vsix redhat.ansible-24.9.320163.vsix
-code --install-extension /opt/FedoraRemix/VSCode/Codeium.codeium-1.17.11.vsix
 code --install-extension /opt/FedoraRemix/VSCode/flobilosaurus.vscode-asciidoc-slides-1.3.0.vsix
 code --install-extension /opt/FedoraRemix/VSCode/ms-python.python-2025.4.0.vsix
 code --install-extension /opt/FedoraRemix/VSCode/MS-vsliveshare.vsliveshare-1.0.5936.vsix
