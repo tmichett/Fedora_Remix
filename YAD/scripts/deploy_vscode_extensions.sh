1380fb38757ea2ec060144acdc83234f513e62af
Travis Michette <tmichett@redhat.com>
2025-04-19 14:28:06 -0400

    FIX: Updated Deploy VSCode script

--- YAD/scripts/deploy_vscode_extensions.sh
+++ YAD/scripts/deploy_vscode_extensions.sh
@@ -1,11 +1,14 @@
 
 
 #!/bin/bash
-code --install-extension /opt/FedoraRemix/VSCode/ChrisChinchilla.vale-vscode-0.21.0.vsix redhat.ansible-24.9.320163.vsix
-code --install-extension /opt/FedoraRemix/VSCode/redhat.vscode-yaml-1.15.0.vsix
-code --install-extension /opt/FedoraRemix/VSCode/flobilosaurus.vscode-asciidoc-slides-1.3.0.vsix
-code --install-extension /opt/FedoraRemix/VSCode/asciidoctor.asciidoctor-vscode-3.3.1.vsix
 code --install-extension /opt/FedoraRemix/VSCode/aaron-bond.better-comments-3.0.2.vsix
 code --install-extension /opt/FedoraRemix/VSCode/adpyke.codesnap-1.3.4.vsix
+code --install-extension /opt/FedoraRemix/VSCode/asciidoctor-vscode-3.4.2.vsix
+code --install-extension /opt/FedoraRemix/VSCode/ChrisChinchilla.vale-vscode-0.21.0.vsix redhat.ansible-24.9.320163.vsix
 code --install-extension /opt/FedoraRemix/VSCode/Codeium.codeium-1.17.11.vsix
+code --install-extension /opt/FedoraRemix/VSCode/flobilosaurus.vscode-asciidoc-slides-1.3.0.vsix
+code --install-extension /opt/FedoraRemix/VSCode/ms-python.python-2025.4.0.vsix
 code --install-extension /opt/FedoraRemix/VSCode/MS-vsliveshare.vsliveshare-1.0.5936.vsix
+code --install-extension /opt/FedoraRemix/VSCode/redhat.ansible-25.3.1.vsix
+code --install-extension /opt/FedoraRemix/VSCode/redhat.vscode-xml-0.28.2025031108@linux-x64.vsix
+code --install-extension /opt/FedoraRemix/VSCode/redhat.vscode-yaml-1.15.0.vsix
